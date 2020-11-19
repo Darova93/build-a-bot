26dbd00ab090ae300a4408aa2f775c09eb7eca12
David Rodriguez <david.rodriguezv@softtek.com>
2020-11-19 00:30:34 -0800

    first commit

---
+++ src/shared/CollapsibleSection.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div class="header">
+      <span @click="open = !open">{{ open ? "&#x25B2; Collapse" : "&#x25BC; Expand" }}</span>
+    </div>
+    <slot v-if="open">
+      <div> Default Content </div>
+    </slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CollapsibleSection',
+  data() {
+    return {
+      open: true,
+    };
+  },
+};
+</script>
+
+<style scoped>
+  .header {
+    background-color: #bbb;
+    padding: 3px;
+    cursor: pointer;
+  }
+</style>
